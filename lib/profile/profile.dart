0033b6c0cac2e851555846c49ab8b9c0d2bc4a80
MultiApollo19 <mariusz.belwon@gmail.com>
2024-08-15 21:50:02 +0200

    Basic profile screen

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,22 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/services/models.dart';
+import 'package:quizapp/shared/shared.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-      body: ElevatedButton(
-          child: Text('signout'),
-          onPressed: () async {
-            await AuthService().signOut();
-            Navigator.of(context)
-                .pushNamedAndRemoveUntil('/', (route) => false);
-          }),
-    );
+    var report = Provider.of<Report>(context);
+    var user = AuthService().user;
+    if (user != null) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(user.displayName ?? 'Guest'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(top: 50),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(user.photoURL ??
+                        'https://www.gravatar.com/avatar/placeholder'),
+                  ),
+                ),
+              ),
+              Text(user.email ?? '',
+                  style: Theme.of(context).textTheme.headlineSmall),
+              const Spacer(),
+              Text('${report.total}',
+                  style: Theme.of(context).textTheme.headlineMedium),
+              Text('Quizzes Completed',
+                  style: Theme.of(context).textTheme.headlineSmall),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () async {
+                    await AuthService().signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/', (route) => false);
+                  },
+                  child: const Text('Logout'))
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Loader();
+    }
   }
 }
